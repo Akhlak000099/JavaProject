8ceedf6c3c3f673934afe8d209e1efdb1ea6d666
Akhlak Ansari <87793134+Akhlak000099@users.noreply.github.com>
2022-06-11 12:45:27 +0530

    Arthmetic Operator

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class arithmetic{
+    public static void main(String args[]) {
+        int num1,num2,res;
+        Scanner op = new Scanner(System.in);
+        System.out.println("Enter two number :");
+        num1 = op.nextInt();
+        num2 = op.nextInt();
+        res = num1+num2;
+        System.out.println("The total is = "+res);
+        res = num1-num2;
+        System.out.println("The total is = "+res);
+        res = num1*num2;
+        System.out.println("The total is = "+res);
+        res = num1/num2;
+        System.out.println("The total is = "+res);
+        res = num1%num2;
+        System.out.println("The total is = "+res);
+        
+        
+    }
+}
