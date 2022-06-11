21f35b94ed936f090d3ae7ebfbc91600feb205c8
Akhlak Ansari <87793134+Akhlak000099@users.noreply.github.com>
2022-06-11 12:47:56 +0530

    Bitwise Operator

---
+++ bitwise.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class bitwise{
+    public static void main(String args[]) {
+        int num1,num2,res;
+        Scanner op = new Scanner(System.in);
+        System.out.println("Enter two number :");
+        num1 = op.nextInt();
+        num2 = op.nextInt();
+        res = num1&num2;
+        System.out.println("The total is = "+res);
+        res = num1^num2;
+        System.out.println("The total is = "+res);
+        res = num1|num2;
+        System.out.println("The total is = "+res);
+       
+        
+    }
+} 
+    
+
