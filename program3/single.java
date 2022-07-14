b71c33152ffa24ce71aa816e510af97dd434e318
Akhlak Ansari <87793134+Akhlak000099@users.noreply.github.com>
2022-07-14 14:13:06 +0530

    Update single.java

--- program3/single.java
+++ program3/single.java
@@ -1,5 +1,5 @@
 package program3;
-
+// WAP to create a class which contain the method Palindrom .the second class will use the proerty of first class and display the output."
 import java.util.Scanner;
 
 public class single{
