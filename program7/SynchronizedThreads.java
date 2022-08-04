d60abc294f5655ace045ae85c5d456390614f265
Akhlak Ansari <87793134+Akhlak000099@users.noreply.github.com>
2022-08-04 21:00:18 +0530

    Thread & MultiThread Program

---
+++ program7/SynchronizedThreads.java
@@ -0,0 +1,31 @@
+package program7;
+
+public class SynchronizedThreads{
+
+	int item;
+	boolean busy = false;
+	synchronized int get(){
+		if (!busy)
+			try{
+				wait();
+			}
+			catch(InterruptedException ie){}
+		System.out.println("Get:" + item);
+		busy = false;
+		notify();
+		return item;
+	}
+	synchronized void put(int item){
+		if (busy)
+			try{
+				wait();
+			}
+			catch(InterruptedException ie){}
+		this.item = item;
+		busy = true;
+		System.out.println("Put:" + item);
+		notify();
+	}
+	
+}
+
